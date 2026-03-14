a3fbc7c0d74f02748cf0ad1884de4937a5d14ced
hieunm3103 <hieunm3103@gmail.com>
2026-03-14 17:15:55 +0700

    refactor(home.nix): remove unused Niri module configuration and settings

--- niri_noctalia/home.nix
+++ niri_noctalia/home.nix
@@ -5,17 +5,6 @@
   home.homeDirectory = "/home/hieunm";
   home.stateVersion = "24.11";
 
-  imports = [
-    inputs.niri.homeModules.niri
-  ];
-
-  programs.niri = {
-    enable = true;
-    settings = {
-      prefer-no-csd = true;
-    };
-  };
-
   home.packages = [
     inputs.noctalia.packages.${pkgs.system}.default
   ];
